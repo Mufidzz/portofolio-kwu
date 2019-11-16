9117ae5ab57b85b658a3963da1f9960918c98009
Mufidzz <dmzmzzz@gmail.com>
2019-11-16 23:32:21 +0700

    release alpha

---
+++ build/precache-manifest.e89773c1d91cb907d4f723701586380e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb14410bcbb9322d7d58336c1ff27f60",
+    "url": "./index.html"
+  },
+  {
+    "revision": "2570f8e4678c489902df",
+    "url": "./static/css/2.833dd627.chunk.css"
+  },
+  {
+    "revision": "506609b986b01c14eb36",
+    "url": "./static/css/main.ae78a5a2.chunk.css"
+  },
+  {
+    "revision": "2570f8e4678c489902df",
+    "url": "./static/js/2.35383210.chunk.js"
+  },
+  {
+    "revision": "506609b986b01c14eb36",
+    "url": "./static/js/main.fa05809b.chunk.js"
+  },
+  {
+    "revision": "a58c9c0bec22c542cb19",
+    "url": "./static/js/runtime-main.22d65aae.js"
+  },
+  {
+    "revision": "3808072d49628e1984fda5d832f557d6",
+    "url": "./static/media/Tech.3808072d.png"
+  },
+  {
+    "revision": "c25445dbddfd48edbe1f28a91e76312b",
+    "url": "./static/media/oflite.c25445db.png"
+  }
+]);
